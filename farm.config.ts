7c8582c5cf74d70e35e2c5562c507421aa6b7a17
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-07-14 18:22:20 -0500

    chore: :wrench: Configurar import alias

--- farm.config.ts
+++ farm.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from "@farmfe/core";
 import farmPluginPostCss from "@farmfe/js-plugin-postcss";
+import path from "path";
 
 export default defineConfig({
   plugins: ["@farmfe/plugin-react", farmPluginPostCss()],
+  compilation: {
+    resolve: {
+      alias: {
+        "@components": path.resolve(process.cwd(), "src/components"),
+        "@interfaces": path.resolve(process.cwd(), "src/interfaces"),
+        "@services": path.resolve(process.cwd(), "src/services"),
+        "@utils": path.resolve(process.cwd(), "src/utils"),
+      },
+    },
+    external: ["node:fs"],
+  },
 });
