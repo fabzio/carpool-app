cb16f2c061c3f9d28cbe2dbe169d2b2d416306b4
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-07-21 21:26:21 -0500

    feat(index): :sparkles: Agregar localización de formateo de fecha

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeProvider from "./contexts/ThemeProvider";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { locale } from "moment";
+import "moment/locale/es-do";
+
+locale("es-do");
 
 const container = document.querySelector("#root");
 const root = createRoot(container!);
