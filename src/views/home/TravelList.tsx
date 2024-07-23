7ce5985963d97a19a7b3a89b4af13ad9a42fd356
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-07-23 14:00:20 -0500

    refactor(home): :recycle: Abstraer listado de viajes en el index

--- src/views/home/TravelList.tsx
+++ src/views/home/TravelList.tsx
@@ -1,26 +1,14 @@
-import { Error, Loading } from "@components";
-import { useSelector } from "@hooks";
-import { useQuery } from "@tanstack/react-query";
 import TravelCard from "./TravelCard";
-import TravelService from "@services/travel.service";
 
-export default function TravelList() {
-  const { user } = useSelector((state) => state.user);
-  const zoneId = user?.zoneId!;
-  const {
-    data: travelListResponse,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ["travels", zoneId],
-    queryFn: TravelService.getTravelList,
-  });
+interface Props {
+  travels: GenericTravel[];
+}
 
-  if (isLoading) return <Loading />;
-  if (isError) return <Error />;
+export default function TravelList({ travels }: Props) {
+
   return (
     <ul className="grid grid-cols-2 gap-2 p-2">
-      {travelListResponse?.data.map((travel: GenericTravel) => (
+      {travels.map((travel: GenericTravel) => (
         <li className="col-span-1" key={travel.travelId}>
           <TravelCard {...travel} />
         </li>
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,17 +1,44 @@
-import { AddButton, TravelModal } from "@components";
+import { AddButton, Error, Loading, TravelModal } from "@components";
 
 import { useState } from "react";
 import TravelList from "./TravelList";
+import { useSelector } from "@hooks";
+import OfferModal from "./OfferModal";
+import { useQuery } from "@tanstack/react-query";
+import TravelService from "@services/travel.service";
 
 export default function Home() {
   const [visible, setVisible] = useState(false);
-  const handleOpen = () => setVisible(true);
-  const handleClose = () => setVisible(false);
+  const { switchBlur } = useSelector((state) => state.theme);
+  const { type,user } = useSelector((state) => state.user);
+  const zoneId = user?.zoneId!;
+  const {
+    data: travelListResponse,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["travels", zoneId],
+    queryFn: TravelService.getTravelList,
+  });
+
+  const handleOpen = () => {
+    switchBlur();
+    setVisible(true);
+  };
+  const handleClose = () => {
+    setVisible(false);
+    switchBlur();
+  };
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
   return (
     <div className="h-full">
-      <TravelList />
+      <TravelList travels={travelListResponse?.data} />
       <AddButton onClick={handleOpen} />
-      <TravelModal visible={visible} handleClose={handleClose} />
+      <TravelModal visible={visible} handleClose={handleClose}>
+        {type === "driver" && <OfferModal handleClose={handleClose} />}
+      </TravelModal>
     </div>
   );
 }
