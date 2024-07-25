9554d26079632d82101c1de6d6ae563e75dcee19
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-07-25 13:03:13 -0500

    style(tailwind.config.ts): :lipstick: Agregar propiedad blur-xs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      blur: {
+        xs: "2px",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
