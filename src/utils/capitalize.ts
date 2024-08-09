dba7dad7a9e3f25a67fec7d8cd9ae39b0c62de97
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-08-09 14:15:24 -0500

    refactor(capitalize): :sparkles: Modificar función para caracteres con espaciado

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,5 +1,8 @@
 export const capitalize = (str: string | undefined): string => {
   if (!str) return "";
-  str = str.toLowerCase();
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 };
