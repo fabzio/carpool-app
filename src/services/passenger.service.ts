ea912432566f89d6593bcecfed1eda0c8f30dfd2
Fabrizio Montoya <fabriziorandall@gmail.com>
2024-08-10 14:19:05 -0500

    feat(travel-detail): :sparkles: Agregar notificación de viaje aceptado a vista de pasajeros

--- src/services/passenger.service.ts
+++ src/services/passenger.service.ts
@@ -58,7 +58,7 @@ class PassengerService {
       if (!res.success) throw new Error(res.message);
       return res.data as TravelRequest["id"];
     } catch (error) {
-      throw new Error();
+      throw new Error((error as Error).message);
     }
   }
 
@@ -77,7 +77,7 @@ class PassengerService {
       if (!res.success) throw new Error(res.message);
       return res.data as Travel["id"];
     } catch (error) {
-      throw new Error();
+      throw new Error((error as Error).message);
     }
   }
 
